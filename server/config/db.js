0d8e88063a9272560ef92d306f708926841db112
mehak kaur <mehakkaurmk21@gmail.com>
2022-10-22 15:11:06 -0400

    Midterm

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    //"URI": "mongodb://localhost/book_store"
+    "URI": "mongodb+srv://admin:Xb5vVPbBBDUDgEUl@cluster0.zrln6lk.mongodb.net/?retryWrites=true&w=majority",
+    "Secret": 'SomeSecret'
+}
